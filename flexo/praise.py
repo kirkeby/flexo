bfba4d027021ccd1009b22bb72e74fd824567ee6
sune.kirkeby <sune.kirkeby@gmail.com>
2007-07-17 19:15:27 +0100

    Replace personal pronouns in !praise and !larts

--- flexo/praise.py
+++ flexo/praise.py
@@ -1,6 +1,36 @@
 # vim:encoding=utf-8
 
+import re
 import random
+
+my_pronouns_re = re.compile(r'\b(mig|min|mine|mit)\b', re.I)
+specials = {
+    'mig': lambda nick: nick,
+}
+
+def replace_pronouns(text, who):
+    start = 0
+    while True:
+        m = my_pronouns_re.search(text, start)
+        if not m:
+            break
+
+        pronoun, = m.groups()
+        if pronoun in specials:
+            replacement = specials[pronoun](who)
+        elif start == 0:
+            if who.endswith('s'):
+                replacement = who
+            else:
+                replacement = who + 's'
+        else:
+            replacement = 'hans'
+
+        s, e = m.span()
+        start = e
+        text = text[:s] + replacement + text[e:]
+
+    return text
 
 class Praiser:
     def __init__(self, bot):
@@ -11,8 +41,7 @@ class Praiser:
         if not command == 'PRIVMSG':
             return
 
-        praiseer = sender.split('!', 1)[0]
-
+        praiseer = self.bot.core.get_nick(sender)
         try:
             where, what, who = rest.split(' ', 2)
         except:
@@ -20,6 +49,8 @@ class Praiser:
 
         if what == (':!' + self.what):
             praises = open(self.path).readlines()
+
+            who = replace_pronouns(who, praiseer)
             praise = random.choice(praises).strip().replace('%s', who)
 
             self.bot.core.action(where, praise)
@@ -32,6 +63,7 @@ class Praiser:
                                     u'Halllo. Der skal være %%s i en %s!'
                                     % self.what)
             else:
+                self.bot.core.got_it(praiseer, where)
                 open(self.path, 'a').write(who + '\n')
             return True
 
