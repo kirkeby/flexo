53aaae46f7f3f81befd25ba16df09dd83f188f4e
sune.kirkeby <sune.kirkeby@gmail.com>
2007-11-10 08:21:42 +0100

    Use a simple text-file not a pickle for factoids

--- flexo/factoid.py
+++ flexo/factoid.py
@@ -1,12 +1,10 @@
 import re
-import pickle
 import random
 
 from flexo.plugin import Plugin
 
-factoids = pickle.load(open('factoids'))
-
-define_re = re.compile(r'^!!\s*(.*)\s+(is|er)\s+(.*)$', re.I)
+define_format = r'^!!\s*(.*?)\s+(er|har)\s+(.*)$'
+define_re = re.compile(define_format, re.I)
 huh = ['Huh?!', 'I know not this %s you speak of',
        'Aner det ikke', 'Kein ahnung']
 
@@ -27,44 +25,47 @@ class Factoid(Plugin):
             return True
 
     def on_lookup(self, sender, where, what):
-        facts = factoids.get(what, None)
-        if facts is None:
+        all = [ l.strip().split('\t', 2) for l in open('factoids') ]
+        facts = [ (how, fact) for key, how, fact in all
+                  if key.lower() == what.lower() ]
+        if not facts:
             reply = random.choice(huh).replace('%s', what)
             self.bot.core.reply(sender, where, reply)
 
         else:
-            text = random.choice(facts)
+            how, text = random.choice(facts)
             if text.startswith('<reply> '):
                 self.bot.send('PRIVMSG %s :%s' % (where, text[8:]))
             elif text.startswith('<action> '):
                 self.bot.core.action(where, text[9:])
             else:
-                txt = 'Jeg mener helt bestemt at %s er %s' % (what, text)
+                txt = 'Jeg mener helt bestemt at %s %s %s' % (what, how, text)
                 self.bot.core.reply(sender, where, txt)
         
     def on_define(self, sender, where, what):
         m = define_re.match(what)
         if not m:
-            txt = '"!!dims is noget om dims" <- FAT DET!'
+            txt = '%r <- FAT DET!' % define_format
             self.bot.core.reply(sender, where, txt)
             return True
 
         thing, how, text = m.groups()
-        if thing in factoids:
-            factoids[thing].append(text)
-        else:
-            factoids[thing] = [text]
-        pickle.dump(factoids, open('factoids', 'w'))
+        thing = thing.replace('\t', ' ')
+        open('factoids', 'a').write('%s\t%s\t%s\n' % (thing, how, text))
+
         self.bot.core.got_it(sender, where)
 
     def on_list(self, sender, where, prefix):
-        facts = []
-        for key in factoids:
+        all = [ l.strip().split('\t', 1) for l in open('factoids') ]
+        facts = {}
+        for key, fact in all:
+            if key.lower() in facts:
+                continue
             if key.lower().startswith(prefix):
-                facts.append(key)
+                facts[key.lower()] = key
 
         if facts:
-            reply = 'Jeg ved ting om ' + ', '.join(facts)
+            reply = 'Jeg ved ting om ' + ', '.join(facts.values())
         else:
             reply = 'No clue'
         self.bot.core.reply(sender, where, reply)
