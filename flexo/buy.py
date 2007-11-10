545d2344187b71ab005be7c1026c3735012789c3
sune.kirkeby <sune.kirkeby@gmail.com>
2007-11-10 08:23:34 +0100

    Use get_nick_channel

--- flexo/buy.py
+++ flexo/buy.py
@@ -1,12 +1,14 @@
 from flexo.plugin import Plugin
+from flexo.prelude import get_nick_channel
 
 class Buy(Plugin):
     def on_cmd_buy(self, context, rest):
         if not isinstance(context, tuple):
             return
 
-        sender, channel = context
-        buyer = sender.split('!')[0][1:]
+        buyer, channel = get_nick_channel(context)
+        if not channel:
+            return
 
         if ' ' in rest:
             who, what = rest.split(' ', 1)
