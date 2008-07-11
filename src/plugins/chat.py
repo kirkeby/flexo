bf46837f6a8daace137709b20c79bb7500fc8e8e
Sune Kirkeby <sune@mel.ibofobi.dk>
2008-07-11 12:50:37 +0200

    Change chat-plugin to be data-driven.

--- src/plugins/chat.py
+++ src/plugins/chat.py
@@ -3,21 +3,24 @@
 import re
 from flexo.plugin import Plugin
 
+def compile(line):
+    pattern, reply = line.strip().split(' -> ', 1)
+    return pattern.strip(), reply.strip()
+replies = [ compile(line.decode('utf-8')) for line in open('state/chat') ]
+
 class Chatter(Plugin):
     def on_privmsg(self, message):
         what = message.says
 
-        if what.startswith(self.bot.nick.lower() + ', '):
-            pass
-        elif what.startswith(self.bot.nick.lower() + ': '):
-            pass
+        nick_pattern = self.bot.nick + '[,:]\\s*'
+        for pattern, reply in replies:
+            pattern = pattern.replace('<nick>', nick_pattern)
+            if re.match(pattern, what, re.I):
+                break
         else:
             return False
 
-        if re.search(r'\bbender\b', what, re.I):
-            message.reply(u'Tell him I hate him!')
-        else:
-            message.reply(u'NOBODY CARES!')
+        message.reply(reply)
         return True
 
 plugin = Chatter
